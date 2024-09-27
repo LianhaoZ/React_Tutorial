5910f22b04591e3c2e8f5720dac2060a02b9b3b0
Lianhao Zheng <lianhaoz06@gmail.com>
2024-09-27 13:16:01 -0500

    Fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,9 @@
-import { useState } from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useJsonQuery } from './utilities/fetch';
 import './App.css';
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }, 
-    "S315" : {
-      "term": "Spring",
-      "number": "334",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }, 
-    "S317" : {
-      "term": "Spring",
-      "number": "324",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
+const queryClient = new QueryClient();
 
 const Banner = ({ title }) => (
   <h1>{title}</h1>
@@ -64,13 +25,25 @@ const CourseList = ({ courses }) => (
   </div>
 );
 
-const App = () => { 
+const Main = () => { 
+  const [courseData, isCourseLoading, courseError] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
+ 
+  if (courseError) return <h1>Error loading course data: {`${courseError}`}</h1>;
+
   return (
-    <div className="container">
-      <Banner title={schedule.title} />
-      <CourseList courses={schedule.courses} />
+    <div>  
+      <Banner title={courseData.title}/> 
+      {courseData?.courses ? <CourseList courses={courseData.courses} /> : <div>No courses available</div>}
     </div>
   );
-};
+}
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <div className="container">
+      <Main />
+    </div>
+  </QueryClientProvider>
+);
 
 export default App;
