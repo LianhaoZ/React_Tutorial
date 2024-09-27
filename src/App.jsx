0c858bedc29e20ec127ca25bdea728aa6e2eb275
Lianhao Zheng <lianhaoz06@gmail.com>
2024-09-27 12:29:31 -0500

    Show a list of courses

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,58 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
 const schedule = {
-  title: "CS Courses for 2018-2019"
+  "title": "CS Courses for 2018-2019",
+  "courses": {
+    "F101" : {
+      "term": "Fall",
+      "number": "101",
+      "meets" : "MWF 11:00-11:50",
+      "title" : "Computer Science: Concepts, Philosophy, and Connections"
+    },
+    "F110" : {
+      "term": "Fall",
+      "number": "110",
+      "meets" : "MWF 10:00-10:50",
+      "title" : "Intro Programming for non-majors"
+    },
+    "S313" : {
+      "term": "Spring",
+      "number": "313",
+      "meets" : "TuTh 15:30-16:50",
+      "title" : "Tangible Interaction Design and Learning"
+    },
+    "S314" : {
+      "term": "Spring",
+      "number": "314",
+      "meets" : "TuTh 9:30-10:50",
+      "title" : "Tech & Human Interaction"
+    }
+  }
 };
+  
+const CourseList = ({ courses }) => (
+  <div className="course-list">
+    {Object.entries(courses).map(([id, course]) => (
+      <div key={id} className="course">
+        {course.term} CS {course.number}: {course.title}
+        <br />
+        <small>Meets: {course.meets}</small>
+      </div>
+    ))}
+  </div>
+);
+
 
 const App = () => { 
 
   return (
-    <div className="App"> 
-      <header className="App-header">
-        <h1>{schedule.title}</h1>
-      </header>
+    <div className="container">
+      <h1>{schedule.title}</h1>
+      <CourseList courses={schedule.courses} />
     </div>
   );
 };
 
 export default App;
+ 
